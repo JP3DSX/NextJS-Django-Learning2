6b48a12571231a8dee2a24e5d9523ee36efc82a3
yutaka.yamamoto <jp3dsx@outlook.jp>
2023-08-30 11:59:20 +0200

    update

--- backend/build.sh
+++ backend/build.sh
@@ -1,9 +1,8 @@
 #!/usr/bin/env bash
 # exit on error
 set -o errexit
-cd ./backend
 pip3 install -r requirements.txt
  
 python3 manage.py collectstatic --noinput
 python3 manage.py migrate
-python3 manage.py createsuperuser --username admin --email admin@test.com --noinput
+python3 manage.py createsuperuser --username admin --email admin@test.com --noinputs
