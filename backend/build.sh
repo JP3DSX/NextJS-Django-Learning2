34081c946b2dc556ff6022506186f675c26def09
yutaka.yamamoto <jp3dsx@outlook.jp>
2023-08-30 09:37:11 +0200

    Render Push

--- backend/build.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-# exit on error
-set -o errexit
- 
-pip install -r requirements.txt
- 
-python manage.py collectstatic --noinput
-python manage.py migrate
-python manage.py createsuperuser --username admin --email admin@test.com --noinput
